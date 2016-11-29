bf0a8ede2dfea21f092abfabee490a0afa09abd2
usuario <usuario@usuario-HP-Compaq-6000-Pro-MT-PC>
2016-11-28 20:25:00 -0500

    clase prgramada por Fabian Ayala

---
+++ Prueba_g2/src/Nuevo/sumar.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nuevo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author usuario
+ */
+public class sumar {
+    
+    public int a;
+    
+    public int b;
+
+    public sumar(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    public void sumar(){
+        
+        int s;
+        
+        s=a+b;
+        
+        
+        JOptionPane.showMessageDialog(null, s);
+        
+    }
+    
+    
+}
